6caf2b64d058f96175c80ec6539e58bf7fc9b126
Jacob <n01628853@humber.ca>
2024-03-28 16:30:02 -0400

    Commit 3

--- src/Feedback.js
+++ src/Feedback.js
@@ -1,10 +1,16 @@
 import { useState } from "react"
+import { SassColor } from "sass"
 const Feedback = () => {
     const [Name, setName] = useState('')
     const [Email, setEmail] = useState('')
+    const [Feedback, setFeedback] = useState('')
+    const [Msg, setMsg] = useState('')
     const Submit = (event) => {
         event.preventDefault()
         console.log(Name);
+        console.log(Email);
+        console.log(Feedback);
+        setMsg("Thank you, your feedback has been received")
     }
     return(
         <div>
@@ -12,10 +18,11 @@ const Feedback = () => {
                 <label>Enter your full name</label><br/>
                 <input type="text" value={Name} onChange={(event) => {setName(event.target.value)}}></input><br/>
                 <label>Enter you email</label><br/>
-                <input type="text"></input><br/>
+                <input type="text" value={Email} onChange={(event) => {setEmail(event.target.value)}}></input><br/>
                 <label>Enter your feedback</label><br/>
-                <input type="text"></input><br/>
-                <button onClick={Submit}>Submit</button>
+                <input type="text" value={Feedback} onChange={(event) => {setFeedback(event.target.value)}}></input><br/>
+                <button onClick={Submit}>Submit</button><br/>
+                <p>{Msg}</p>
             </form>
         </div>
     )
