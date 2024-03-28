cbb798597eda4c77282369d790b3ebe4860e7495
Jacob <n01628853@humber.ca>
2024-03-28 16:07:36 -0400

    Commit 2

--- src/Feedback.js
+++ src/Feedback.js
@@ -1,15 +1,21 @@
+import { useState } from "react"
 const Feedback = () => {
+    const [Name, setName] = useState('')
+    const [Email, setEmail] = useState('')
+    const Submit = (event) => {
+        event.preventDefault()
+        console.log(Name);
+    }
     return(
         <div>
             <form>
-                <label>Enter your first name</label><br/>
-                <input type="text"></input><br/>
-                <label>Enter your last name</label><br/>
-                <input type="text"></input><br/>
+                <label>Enter your full name</label><br/>
+                <input type="text" value={Name} onChange={(event) => {setName(event.target.value)}}></input><br/>
                 <label>Enter you email</label><br/>
                 <input type="text"></input><br/>
                 <label>Enter your feedback</label><br/>
-                <input type="text"></input>
+                <input type="text"></input><br/>
+                <button onClick={Submit}>Submit</button>
             </form>
         </div>
     )
