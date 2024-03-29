e52448a4620b90f74bde600ae25e62fd94c13474
N01645357 <N01645357@humber.ca>
2024-03-28 22:00:48 -0400

    base structure of the currency converter.

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -2,18 +2,63 @@ import { useEffect, useState } from "react"
 import axios from "axios"
 
 const Converter = () => {
-
-    
     return (
         <div>
             <h3>Currency Converter</h3>
-            
+            {/* fix currency names to be like this: Singapore Dollar (SGD) */}
+            <select>
+                <option value="">Select Currency</option>
+                <option value="Singapore Dollar">SGD</option>
+                <option value="Malaysian Ringgit">MYR</option>
+                <option value="Euro">EUR</option>
+                <option value="United States Dollar">USD</option>
+                <option value="Australian Dollar">AUD</option>
+                <option value="Japanese Yen">JPY</option>
+                <option value="Chinese Yuan">CNY</option>
+                <option value="Hong Kong Dollar">HKD</option>
+                <option value="Canadian Dollar">CAD</option>
+                <option value="Indian Rupee">INR</option>
+                <option value="Danish Krone">DKK</option>
+                <option value="Great Britain Pound">GBP</option>
+                <option value="Russian Ruble">RUB</option>
+                <option value="New Zealand Dollar">NZD</option>
+                <option value="Mexican Peso">MXN</option>
+                <option value="Indonesian Rupiah">IDR</option>
+                <option value="Taiwanese Dollar">NTD</option>
+                <option value="Thai Baht">THB</option>
+                <option value="Vietnamese Dong">VND</option>
+            </select>
+            <input
+                type="number"
+                placeholder="amount"/>
+            <br/><br/>
+            <select>
+            <option value="">Select Currency</option>
+                <option value="Singapore Dollar">SGD</option>
+                <option value="Malaysian Ringgit">MYR</option>
+                <option value="Euro">EUR</option>
+                <option value="United States Dollar">USD</option>
+                <option value="Australian Dollar">AUD</option>
+                <option value="Japanese Yen">JPY</option>
+                <option value="Chinese Yuan">CNY</option>
+                <option value="Hong Kong Dollar">HKD</option>
+                <option value="Canadian Dollar">CAD</option>
+                <option value="Indian Rupee">INR</option>
+                <option value="Danish Krone">DKK</option>
+                <option value="Great Britain Pound">GBP</option>
+                <option value="Russian Ruble">RUB</option>
+                <option value="New Zealand Dollar">NZD</option>
+                <option value="Mexican Peso">MXN</option>
+                <option value="Indonesian Rupiah">IDR</option>
+                <option value="Taiwanese Dollar">NTD</option>
+                <option value="Thai Baht">THB</option>
+                <option value="Vietnamese Dong">VND</option>
+            </select>
+            <input
+                type="number"
+                placeholder="amount"/>
         </div>
     )
 }
 
 export default Converter
-
-// API: https://rapidapi.com/fyhao/api/currency-exchange
-// Documentation: https://rapidapi.com/blog/currency-exchange-api-with-php-python-ruby-javascript-examples/
-// API Key: bdfe3bda68msh3027425b6f0a1e1p1d60cfjsn33b00a3ba96c
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,8 @@ const Home = () => {
     return(
         <div>
             <h1><b><u>CURRENCY CONVERSION</u></b></h1><br/>
-            <p>Welcome to our currency conversion website</p>
-            <p>The goal of this Website is to make currency conversion simple and easy for the avarage user</p>
+            <p>Welcome to our currency conversion website.</p>
+            <p>The goal of this Website is to make currency conversion simple and easy for the average user.</p>
         </div>
     )
 }
